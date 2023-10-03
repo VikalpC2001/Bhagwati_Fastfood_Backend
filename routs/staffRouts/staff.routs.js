891f35341b3d9d7b13ac11eb5eca4989698166ca
VikalpC2001 <vikalp0810@gmail.com>
2023-10-03 17:57:07 +0530

    add protect

--- routs/staffRouts/staff.routs.js
+++ routs/staffRouts/staff.routs.js
@@ -7,7 +7,7 @@ const { protect } = require("../../middlewares/authMiddlewares.js");
 const employeeController = require("../../controller/staffController/employee.controller.js");
 
 router.post('/addEmployeedetails', protect, employeeController.addEmployeedetails);
-router.get('/getImagebyName', employeeController.getImagebyName);
+router.get('/getImagebyName', protect, employeeController.getImagebyName);
 router.delete('/removeEmployeeDetails', protect, employeeController.removeEmployeeDetails);
 router.post('/updateEmployeeDetails', protect, employeeController.updateEmployeeDetails);
 router.get('/fillEmployeeDetails', protect, employeeController.fillEmployeeDetails);
@@ -21,13 +21,13 @@ router.get('/getEmployeeIdAndName', employeeController.getEmployeeIdAndName);
 
 const staffCategoryController = require("../../controller/staffController/staffCategory.controller.js");
 
-router.post('/addStaffCategory', staffCategoryController.addStaffCategory);
-router.delete('/removeStaffCategory', staffCategoryController.removeStaffCategory);
-router.get('/ddlStaffCategory', staffCategoryController.ddlStaffCategory);
-router.get('/getStaffCategoryList', staffCategoryController.getStaffCategoryList);
-router.post('/updateStaffCategory', staffCategoryController.updateStaffCategory);
-router.get('/getStaffCategoryWithEmployeeNumber', staffCategoryController.getStaffCategoryWithEmployeeNumber);
-router.get('/getEmployeeStatisticsByCategoryId', staffCategoryController.getEmployeeStatisticsByCategoryId);
+router.post('/addStaffCategory', protect, staffCategoryController.addStaffCategory);
+router.delete('/removeStaffCategory', protect, staffCategoryController.removeStaffCategory);
+router.get('/ddlStaffCategory', protect, staffCategoryController.ddlStaffCategory);
+router.get('/getStaffCategoryList', protect, staffCategoryController.getStaffCategoryList);
+router.post('/updateStaffCategory', protect, staffCategoryController.updateStaffCategory);
+router.get('/getStaffCategoryWithEmployeeNumber', protect, staffCategoryController.getStaffCategoryWithEmployeeNumber);
+router.get('/getEmployeeStatisticsByCategoryId', protect, staffCategoryController.getEmployeeStatisticsByCategoryId);
 
 // Salary ,Fine and Advance Routs
 
@@ -35,7 +35,7 @@ const sallaryController = require("../../controller/staffController/salary.contr
 
 router.post('/addAmountOfSFA', protect, sallaryController.addAmountOfSFA);
 router.delete('/removeCreditTransaction', protect, sallaryController.removeCreditTransaction);
-router.delete('/removeSalaryTranction', sallaryController.removeSalaryTranction);
+router.delete('/removeSalaryTranction', protect, sallaryController.removeSalaryTranction);
 router.post('/updateEmployeeStatus', protect, sallaryController.updateEmployeeStatus, sallaryController.addAmountOfSFA);
 router.get('/updateFineStatus', protect, sallaryController.updateFineStatus);
 router.get('/updateFineTransaction', protect, sallaryController.updateFineTransaction);
@@ -43,14 +43,14 @@ router.delete('/removeAdvanceTransaction', protect, sallaryController.removeAdva
 router.delete('/removeFineTransaction', protect, sallaryController.removeFineTransaction);
 router.delete('/removeBonusTransaction', protect, sallaryController.removeBonusTransaction);
 router.delete('/removeMonthlySalary', protect, sallaryController.removeMonthlySalary);
-router.get('/updateMonthlySalary', sallaryController.updateMonthlySalary);
+router.get('/updateMonthlySalary', protect, sallaryController.updateMonthlySalary);
 
 // Leave Routs
 
 const leaveController = require('../../controller/staffController/leave.controller.js')
 
-router.post('/addEmployeeLeave', leaveController.addEmployeeLeave);
-router.post('/addLeaveForAllEployee', leaveController.addLeaveForAllEmployee);
+router.post('/addEmployeeLeave', protect, leaveController.addEmployeeLeave);
+router.post('/addLeaveForAllEployee', protect, leaveController.addLeaveForAllEmployee);
 router.delete('/removeEmployeeLeave', protect, leaveController.removeEmployeeLeave);
 router.post('/updateEmployeeLeave', protect, leaveController.updateEmployeeLeave);
 router.delete('/removeEmployeeHoliday', protect, leaveController.removeEmployeeHoliday);
@@ -59,75 +59,75 @@ router.delete('/removeEmployeeHoliday', protect, leaveController.removeEmployeeH
 
 const employeeTableController = require('../../controller/staffController/employeeTable.controller.js');
 
-router.get('/getEmployeeMonthlySalaryById', employeeTableController.getEmployeeMonthlySalaryById);
-router.get('/getAdvanceDataById', employeeTableController.getAdvanceDataById);
-router.get('/getFineDataById', employeeTableController.getFineDataById);
-router.get('/getBonusDataById', employeeTableController.getBonusDataById);
-router.get('/getCreditDataById', employeeTableController.getCreditDataById);
-router.get('/getLeaveDataById', employeeTableController.getLeaveDataById);
-router.get('/getTransactionDataById', employeeTableController.getTransactionDataById);
-router.get('/getCutSalaryDataById', employeeTableController.getCutSalaryDataById);
-router.get('/getAllPaymentStatisticsCountById', employeeTableController.getAllPaymentStatisticsCountById);
-router.get('/getCutCreditDataById', employeeTableController.getCutCreditDataById);
-router.get('/getPresentDaysByEmployeeId', employeeTableController.getPresentDaysByEmployeeId);
+router.get('/getEmployeeMonthlySalaryById', protect, employeeTableController.getEmployeeMonthlySalaryById);
+router.get('/getAdvanceDataById', protect, employeeTableController.getAdvanceDataById);
+router.get('/getFineDataById', protect, employeeTableController.getFineDataById);
+router.get('/getBonusDataById', protect, employeeTableController.getBonusDataById);
+router.get('/getCreditDataById', protect, employeeTableController.getCreditDataById);
+router.get('/getLeaveDataById', protect, employeeTableController.getLeaveDataById);
+router.get('/getTransactionDataById', protect, employeeTableController.getTransactionDataById);
+router.get('/getCutSalaryDataById', protect, employeeTableController.getCutSalaryDataById);
+router.get('/getAllPaymentStatisticsCountById', protect, employeeTableController.getAllPaymentStatisticsCountById);
+router.get('/getCutCreditDataById', protect, employeeTableController.getCutCreditDataById);
+router.get('/getPresentDaysByEmployeeId', protect, employeeTableController.getPresentDaysByEmployeeId);
 
 // Export Table Data Routs
 
 const exportDataTable = require('../../controller/staffController/exportDataTable.controller.js');
 
 // Export Excel For Employee Data
-router.get('/exportExcelSheetForEmployeeMonthlySalaryDataById', exportDataTable.exportExcelSheetForEmployeeMonthlySalaryDataById);
-router.get('/exportExcelSheetForAdvanceData', exportDataTable.exportExcelSheetForAdvanceData);
-router.get('/exportExcelSheetForFineData', exportDataTable.exportExcelSheetForFineData);
-router.get('/exportExcelSheetForBonusData', exportDataTable.exportExcelSheetForBonusData);
-router.get('/exportExcelSheetForCreditData', exportDataTable.exportExcelSheetForCreditData);
-router.get('/exportExcelSheetForLeaveData', exportDataTable.exportExcelSheetForLeaveData);
-router.get('/exportExcelSheetForTransactionData', exportDataTable.exportExcelSheetForTransactionData);
+router.get('/exportExcelSheetForEmployeeMonthlySalaryDataById', protect, exportDataTable.exportExcelSheetForEmployeeMonthlySalaryDataById);
+router.get('/exportExcelSheetForAdvanceData', protect, exportDataTable.exportExcelSheetForAdvanceData);
+router.get('/exportExcelSheetForFineData', protect, exportDataTable.exportExcelSheetForFineData);
+router.get('/exportExcelSheetForBonusData', protect, exportDataTable.exportExcelSheetForBonusData);
+router.get('/exportExcelSheetForCreditData', protect, exportDataTable.exportExcelSheetForCreditData);
+router.get('/exportExcelSheetForLeaveData', protect, exportDataTable.exportExcelSheetForLeaveData);
+router.get('/exportExcelSheetForTransactionData', protect, exportDataTable.exportExcelSheetForTransactionData);
 
 // Export Excel For All Table Data
-router.get('/exportExcelSheetForAllTransactionData', exportDataTable.exportExcelSheetForAllTransactionData);
-router.get('/exportExcelSheetForAllAdvanceData', exportDataTable.exportExcelSheetForAllAdvanceData);
-router.get('/exportExcelSheetForAllFineData', exportDataTable.exportExcelSheetForAllFineData);
-router.get('/exportExcelSheetForAllBonusData', exportDataTable.exportExcelSheetForAllBonusData);
-router.get('/exportExcelSheetForAllCreditData', exportDataTable.exportExcelSheetForAllCreditData);
-router.get('/exportExcelSheetForAllLeaveData', exportDataTable.exportExcelSheetForAllLeaveData);
+router.get('/exportExcelSheetForAllTransactionData', protect, exportDataTable.exportExcelSheetForAllTransactionData);
+router.get('/exportExcelSheetForAllAdvanceData', protect, exportDataTable.exportExcelSheetForAllAdvanceData);
+router.get('/exportExcelSheetForAllFineData', protect, exportDataTable.exportExcelSheetForAllFineData);
+router.get('/exportExcelSheetForAllBonusData', protect, exportDataTable.exportExcelSheetForAllBonusData);
+router.get('/exportExcelSheetForAllCreditData', protect, exportDataTable.exportExcelSheetForAllCreditData);
+router.get('/exportExcelSheetForAllLeaveData', protect, exportDataTable.exportExcelSheetForAllLeaveData);
 
 // Export PDF For All Table Data
-router.get('/exportPdfForAllTransactionData', exportDataTable.exportPdfForAllTransactionData);
-router.get('/exportPdfForAllAdvanceData', exportDataTable.exportPdfForAllAdvanceData);
-router.get('/exportPdfForALLFineData', exportDataTable.exportPdfForALLFineData);
-router.get('/exportPdfForAllBonusData', exportDataTable.exportPdfForAllBonusData);
-router.get('/exportPdfForAllCreditData', exportDataTable.exportPdfForAllCreditData);
-router.get('/exportPdfForAllLeaveData', exportDataTable.exportPdfForAllLeaveData);
+router.get('/exportPdfForAllTransactionData', protect, exportDataTable.exportPdfForAllTransactionData);
+router.get('/exportPdfForAllAdvanceData', protect, exportDataTable.exportPdfForAllAdvanceData);
+router.get('/exportPdfForALLFineData', protect, exportDataTable.exportPdfForALLFineData);
+router.get('/exportPdfForAllBonusData', protect, exportDataTable.exportPdfForAllBonusData);
+router.get('/exportPdfForAllCreditData', protect, exportDataTable.exportPdfForAllCreditData);
+router.get('/exportPdfForAllLeaveData', protect, exportDataTable.exportPdfForAllLeaveData);
 
 // Export PDF For Employee Data
-router.get('/exportPdfForEmployeeMonthlySalaryData', exportDataTable.exportPdfForEmployeeMonthlySalaryData);
-router.get('/exportPdfForAdvanceData', exportDataTable.exportPdfForAdvanceData);
-router.get('/exportPdfForFineData', exportDataTable.exportPdfForFineData);
-router.get('/exportPdfForBonusData', exportDataTable.exportPdfForBonusData);
-router.get('/exportPdfForCreditData', exportDataTable.exportPdfForCreditData);
-router.get('/exportPdfForLeaveData', exportDataTable.exportPdfForLeaveData);
-router.get('/exportPdfForTransactionData', exportDataTable.exportPdfForTransactionData);
+router.get('/exportPdfForEmployeeMonthlySalaryData', protect, exportDataTable.exportPdfForEmployeeMonthlySalaryData);
+router.get('/exportPdfForAdvanceData', protect, exportDataTable.exportPdfForAdvanceData);
+router.get('/exportPdfForFineData', protect, exportDataTable.exportPdfForFineData);
+router.get('/exportPdfForBonusData', protect, exportDataTable.exportPdfForBonusData);
+router.get('/exportPdfForCreditData', protect, exportDataTable.exportPdfForCreditData);
+router.get('/exportPdfForLeaveData', protect, exportDataTable.exportPdfForLeaveData);
+router.get('/exportPdfForTransactionData', protect, exportDataTable.exportPdfForTransactionData);
 
 // Employee Invoice Routs
 
 const employeeInvoiceController = require('../../controller/staffController/empInvoice.controller.js');
 const { RotationTypes } = require('pdf-lib');
 
-router.get('/getEmployeeInvoice', employeeInvoiceController.getEmployeeInvoice);
+router.get('/getEmployeeInvoice', protect, employeeInvoiceController.getEmployeeInvoice);
 
 // All Payment Routs
 
 const allPaymentController = require('../../controller/staffController/allPayment.controller.js');
 
-router.get('/getAllEmployeeTransactionData', allPaymentController.getAllEmployeeTransactionData);
-router.get('/getAllEmployeeLeaveData', allPaymentController.getAllEmployeeLeaveData);
-router.get('/getAllEmployeeBonusData', allPaymentController.getAllEmployeeBonusData);
-router.get('/getAllEmployeeCreditData', allPaymentController.getAllEmployeeCreditData);
-router.get('/getAllEmployeeFineData', allPaymentController.getAllEmployeeFineData);
-router.get('/getAllEmployeeAdvanceData', allPaymentController.getAllEmployeeAdvanceData);
-router.get('/getAllPaymentStatisticsCount', allPaymentController.getAllPaymentStatisticsCount);
-router.get('/getAllEmployeeHolidayData', allPaymentController.getAllEmployeeHolidayData);
+router.get('/getAllEmployeeTransactionData', protect, allPaymentController.getAllEmployeeTransactionData);
+router.get('/getAllEmployeeLeaveData', protect, allPaymentController.getAllEmployeeLeaveData);
+router.get('/getAllEmployeeBonusData', protect, allPaymentController.getAllEmployeeBonusData);
+router.get('/getAllEmployeeCreditData', protect, allPaymentController.getAllEmployeeCreditData);
+router.get('/getAllEmployeeFineData', protect, allPaymentController.getAllEmployeeFineData);
+router.get('/getAllEmployeeAdvanceData', protect, allPaymentController.getAllEmployeeAdvanceData);
+router.get('/getAllPaymentStatisticsCount', protect, allPaymentController.getAllPaymentStatisticsCount);
+router.get('/getAllEmployeeHolidayData', protect, allPaymentController.getAllEmployeeHolidayData);
 
 module.exports = router;
 
